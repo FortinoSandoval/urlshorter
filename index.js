b02637db1d5ae43b5d20121a1538c200eb15f856
Fortino Sandoval <Fortysc97@gmail.com>
2019-10-10 23:37:59 -0700

    pool

--- index.js
+++ index.js
@@ -14,42 +14,41 @@ app.use(express.static('dist'));
 app.set('view engine', 'ejs');
 
 // mysql Connection
-const connection = mysql.createConnection({
-  host     : '162.241.60.122',
-  user     : 'techcour_adm',
-  password : 'Fortielchilo',
-  database : 'techcour_raw'
+const connection = mysql.createPool({
+  connectionLimit :  10,
+  host            : '162.241.60.122',
+  user            : 'techcour_adm',
+  password        : 'Fortielchilo',
+  database        : 'techcour_raw'
 });
-
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.post('/getcourse/:name', (req, res) => {
-  connection.connect();
-  const originalName = Buffer.from(req.params.name, 'base64').toString();
-  connection.query(`SELECT * FROM tnt_file where file_name = '${originalName}'`, (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send(err)
-    } else {
-      if (result.length) {
-        var fileData = new Buffer.from(result[0].tnt);
-        res.writeHead(200, {
-          'Content-Type': result[0].type,
-          'Content-Disposition': `attachment; filename=${originalName}.torrent`,
-          'Content-Length': fileData.length
-        });
-        res.write(fileData);
-        res.end();
+  connection.getConnection((err, connection) => {
+    const originalName = Buffer.from(req.params.name, 'base64').toString();
+    connection.query(`SELECT * FROM tnt_file where file_name = '${originalName}'`, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send(err)
       } else {
-        res.send('something went wrong');
+        if (result.length) {
+          var fileData = new Buffer.from(result[0].tnt);
+          res.writeHead(200, {
+            'Content-Type': result[0].type,
+            'Content-Disposition': `attachment; filename=${originalName}.torrent`,
+            'Content-Length': fileData.length
+          });
+          res.write(fileData);
+          res.end();
+        } else {
+          res.send('something went wrong');
+        }
       }
-    }
-  });
-  connection.end();
+    });
+  })
 });
 
 app.listen(process.env.PORT || 4000, () => {
